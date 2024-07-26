8f334267a2e40d37be75436609989306592b4885
Leonid Averyanov <averyanov.l@promail366.com>
2024-07-26 05:38:41 +0400

    resque api

--- src/Service/Review/ContentProvider/ImageProvider.php
+++ src/Service/Review/ContentProvider/ImageProvider.php
@@ -4,6 +4,7 @@ namespace AverianovL\GroomingBot\Service\Review\ContentProvider;
 
 use AverianovL\GroomingBot\Service\Providers\ImgurClientProvider;
 use AverianovL\GroomingBot\Service\Review\Object\Image;
+use GuzzleHttp\Client as Guzzle;
 
 class ImageProvider
 {
@@ -12,33 +13,40 @@ class ImageProvider
 
     public static function getImage(): Image
     {
-        $imgurClient = ImgurClientProvider::get();
+        try {
+            $imgurClient = ImgurClientProvider::get();
 
-        $getLinkIfImage = static function ($item): ?string {
-            if (in_array($item['type'] ?? false, ['image/jpeg', 'image/png'])) {
-                return $item['link'] ?? null;
-            }
+            $getLinkIfImage = static function ($item): ?string {
+                if (in_array($item['type'] ?? false, ['image/jpeg', 'image/png'])) {
+                    return $item['link'] ?? null;
+                }
 
-            return null;
-        };
+                return null;
+            };
 
-        $searchResults =  $imgurClient->api('gallery')->search(self::REQUEST_TEST, self::REQUEST_SORT);
-        // filter to images only
-        $filtered = [];
-        foreach ($searchResults as $searchResult) {
-            $filtered[] = $getLinkIfImage($searchResult);
+            $searchResults =  $imgurClient->api('gallery')->search(self::REQUEST_TEST, self::REQUEST_SORT);
+            // filter to images only
+            $filtered = [];
+            foreach ($searchResults as $searchResult) {
+                $filtered[] = $getLinkIfImage($searchResult);
 
-            if ($searchResult['is_album'] ?? false) {
-                $albumImages = $imgurClient->api('album')->albumImages($searchResult['id']);
+                if ($searchResult['is_album'] ?? false) {
+                    $albumImages = $imgurClient->api('album')->albumImages($searchResult['id']);
 
-                foreach ($albumImages as $albumImage) {
-                    $filtered[] = $getLinkIfImage($albumImage);
+                    foreach ($albumImages as $albumImage) {
+                        $filtered[] = $getLinkIfImage($albumImage);
+                    }
                 }
             }
+
+            $filtered = array_merge(array_filter($filtered));
+
+            return new Image($filtered[rand(0, count($filtered) - 1)]);
+        } catch (\Throwable $e) {
+            $apiResultRaw = (new Guzzle())->request('GET', 'https://dog.ceo/api/breeds/image/random', [])->getBody();
+            $apiResult = json_decode((string) $apiResultRaw, true);
+
+            return new Image($apiResult['message']);
         }
-
-        $filtered = array_merge(array_filter($filtered));
-
-        return new Image($filtered[rand(0, count($filtered) - 1)]);
     }
 }
